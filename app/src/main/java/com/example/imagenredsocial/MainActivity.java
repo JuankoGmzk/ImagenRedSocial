8bd15a62fffb6deb476a1af973d94d54aa0e1e0a
JuankoGmzk <37718099+JuankoGmzk@users.noreply.github.com>
2019-11-30 19:55:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/imagenredsocial/MainActivity.java
@@ -0,0 +1,186 @@
+package com.example.imagenredsocial;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btChoose, btUpload;
+
+    EditText etName;
+    ImageView ivResult;
+    TextView tvUrl;
+
+
+    private static final int Selected = 100;
+    Uri uriImage;
+
+    FirebaseStorage storage;
+    StorageReference storageRef,imageRef;
+    ProgressDialog progressDialog;
+    UploadTask uploadTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(MainActivity.this);
+        btChoose = findViewById(R.id.bt_choose);
+        btUpload = findViewById(R.id.bt_Upload);
+        etName = findViewById(R.id.et_name);
+        ivResult = findViewById(R.id.iv_result);
+        tvUrl = findViewById(R.id.tv_url);
+
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+
+
+        btChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), Selected);
+            }
+        });
+
+
+
+        etName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btUpload.setEnabled(true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                UploadImage();
+
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case Selected:
+                if (resultCode == RESULT_OK){
+                    assert data != null;
+                    uriImage = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uriImage);
+                        ivResult.setImageBitmap(bitmap);
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+                }
+        }
+    }
+
+    private void UploadImage() {
+
+        imageRef = storageRef.child("Images/"+etName.getText().toString()+"."+
+                GetExtension(uriImage));
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMax(100);
+        progressDialog.setMessage("Uploading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+
+
+        uploadTask= imageRef.putFile(uriImage);
+
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred())/
+                        taskSnapshot.getTotalByteCount();
+                progressDialog.incrementProgressBy((int) progress);
+            }
+        });
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Failed !", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(),"SuccesFully Uploaded !!",Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+                Uri downloadUrl = taskSnapshot.getUploadSessionUri();
+                String ImageUrl = downloadUrl.toString();
+                tvUrl.setText(ImageUrl);
+            }
+        });
+
+
+    }
+
+    private String GetExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap= MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+}
